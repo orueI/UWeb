e547b1e82f7a436fb2ad0aca1161b85004169abd
Michael <misha@part-iskra.org>
2021-03-21 16:11:09 +0200

    Finish Sprint00

---
+++ MarathonWEB/Sprint02/t07/js/script.js
@@ -0,0 +1,11 @@
+const arr = [7, 2, 15, 5, 1, 3, 8, 1, 10, 7, 11]
+console.log(arr)
+console.log(sortEvenOld(arr))
+console.log(arr)
+
+function sortEvenOld(arr) {
+    return arr.sort((i,i1)=>{
+        i<i1
+    })
+    // return arr
+}
